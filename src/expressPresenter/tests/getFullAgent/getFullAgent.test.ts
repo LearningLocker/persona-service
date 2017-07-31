26ba134229547784cd9ffa33bf52520b3ad12a10
Chris Bishop <chrisjbishop155@gmail.com>
2017-07-31 15:16:25 +0100

    Adds express tests for getting full agents.

---
+++ src/expressPresenter/tests/getFullAgent/getFullAgent.test.ts
@@ -0,0 +1,58 @@
+import * as express from 'express';
+import * as stringToStream from 'string-to-stream';
+import * as supertest from 'supertest';
+import serviceFactory from '../../../serviceFactory';
+import {
+  TEST_CLIENT,
+  TEST_CONTENT,
+  TEST_INVALID_AGENT,
+  TEST_MBOX_AGENT,
+  TEST_PROFILE_ID,
+  TEXT_CONTENT_TYPE,
+} from '../../../tests/utils/values';
+import {
+  BAD_REQUEST_400_HTTP_CODE,
+  OK_200_HTTP_CODE,
+} from '../../utils/httpCodes';
+import createExpressPresenterFacade from '../utils/createExpressPresenterFacade';
+
+const app = express();
+const service = serviceFactory();
+const expressPresenter = createExpressPresenterFacade(service);
+app.use(expressPresenter);
+
+describe('GET /xAPI/agents', () => {
+  it('should respond with 200 when full agent not found', async () => {
+    await supertest(app)
+      .get(`/xAPI/agents`)
+      .query({
+        agent: JSON.stringify(TEST_MBOX_AGENT),
+      })
+      .expect(OK_200_HTTP_CODE);
+  });
+
+  it('should respond with 200 when full agent exists', async () => {
+    await service.overwriteProfile({
+      agent: TEST_MBOX_AGENT,
+      client: TEST_CLIENT,
+      content: stringToStream(TEST_CONTENT),
+      contentType: TEXT_CONTENT_TYPE,
+      profileId: TEST_PROFILE_ID,
+    });
+    await supertest(app)
+      .get(`/xAPI/agents`)
+      .query({
+        agent: JSON.stringify(TEST_MBOX_AGENT),
+      })
+      .expect(OK_200_HTTP_CODE);
+  });
+
+  it('should respond with 400 when agent is not valid', async () => {
+    await supertest(app)
+      .get(`/xAPI/agents`)
+      .query({
+        agent: JSON.stringify(TEST_INVALID_AGENT),
+      })
+      .expect(BAD_REQUEST_400_HTTP_CODE);
+  });
+});
