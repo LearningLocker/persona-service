07f4aed956cc704b2f612f686740d207cea11a8c
Chris Bishop <chrisjbishop155@gmail.com>
2017-10-17 12:33:33 +0100

    mr comments.

--- src/service/overwritePersonaAttribute.ts
+++ src/service/overwritePersonaAttribute.ts
@@ -1,10 +1,9 @@
+// tslint:disable-next-line:no-unused
 import OverwritePersonaAttributeOptions from // tslint:disable-line:import-spacing
   '../serviceFactory/options/OverwritePersonaAttributeOptions';
+// tslint:disable-next-line:no-unused
 import OverwritePersonaAttributeResult from // tslint:disable-line:import-spacing
   '../serviceFactory/results/OverwritePersonaAttributeResult';
 import Config from './Config';
 
-export default (config: Config) =>
-  async (opts: OverwritePersonaAttributeOptions): Promise<OverwritePersonaAttributeResult> => {
-    return config.repo.overwritePersonaAttribute(opts);
-  };
+export default (config: Config) => config.repo.overwritePersonaAttribute;
