d12e1027e5c61f04169ca1fb93d5c43fc9e20eb4
Chris Bishop <chrisjbishop155@gmail.com>
2017-11-03 13:51:56 +0000

    Split out file.

--- src/tests/getAttributes/getAttributes.test.ts
+++ src/tests/getAttributes/getAttributes.test.ts
@@ -1,67 +1,15 @@
 import * as assert from 'assert';
-import assertError from 'jscommons/dist/tests/utils/assertError';
-import { map, times } from 'lodash';
-import NoCursorBackwardsDirection from '../../errors/NoCursorBackwardsDirection';
-import Attribute from '../../models/Attribute';
 import { modelToCursor } from '../../repoFactory/utils/cursor';
-import OverwritePersonaAttributeResult from // tslint:disable-line:import-spacing
-  '../../serviceFactory/results/OverwritePersonaAttributeResult';
-import GetOptions, { CursorDirection } from '../../serviceFactory/utils/GetOptions';
-import createTestPersona from '../utils/createTestPersona';
+import { CursorDirection } from '../../serviceFactory/utils/GetOptions';
+import { addTestAttributes, getAttributesOptions } from '../utils/getAttributes';
 import setup from '../utils/setup';
-import {
-  TEST_ORGANISATION,
-} from '../utils/values';
+import { TEST_ORGANISATION } from '../utils/values';
 
-describe('getAttributes', () => {
+describe('getAttributes forwards', () => {
   const service = setup();
 
-  const getAttributesOptions = {
-    direction: CursorDirection.FORWARDS,
-    filter: {},
-    limit: 10,
-    maxScan: 0,
-    maxTimeMS: 0,
-    organisation: TEST_ORGANISATION,
-    project: {},
-    sort: {
-      value: 1,
-    },
-  } as GetOptions;
-
-  const fromFirstCursor = modelToCursor({
-    model: {
-      value: 'brown11',
-    },
-    sort: {
-      value: 1,
-    },
-  });
-
-  const addTestAttributes = async () => {
-    const persona = await createTestPersona();
-
-    const NUM_IDENTIFERS = 12;
-    const resultsPromise: Promise<OverwritePersonaAttributeResult>[] = times(NUM_IDENTIFERS,
-      (i) => {
-        return service.overwritePersonaAttribute({
-          key: `hair${i}`,
-          organisation: TEST_ORGANISATION,
-          personaId: persona.id,
-          value: `brown${i}`,
-        });
-      },
-    );
-
-    const results: ArrayLike<OverwritePersonaAttributeResult> = await Promise.all(resultsPromise);
-
-    return map<OverwritePersonaAttributeResult, Attribute>(results, ({attribute}) => attribute );
-  };
-
   it('Should return the first 10 items', async () => {
-
-    // Add 12 Attributes
-    const attributes = await addTestAttributes(); // tslint:disable-line
+    await addTestAttributes(service); // Add 12 Attributes
 
     // Get the first 10 attributes
     const attributesResults = await service.getAttributes(
@@ -81,10 +29,8 @@ describe('getAttributes', () => {
         },
       },
     );
-
     const TEN = 10;
     assert.equal(attributesResults.edges.length, TEN);
-
     assert.equal(attributesResults.edges[0].node.value, 'brown0');
     assert.equal(
       attributesResults.edges[attributesResults.edges.length - 1].node.value,
@@ -115,87 +61,8 @@ describe('getAttributes', () => {
     );
   });
 
-  it('Should return the last 2 attributes', async () => {
-
-    const attributes = await addTestAttributes(); // tslint:disable-line
-
-    const fromCursor = modelToCursor({
-      model: {
-        value: 'brown7',
-      },
-      sort: {
-        value: 1,
-      },
-    });
-
-    const attributesResults = await service.getAttributes({
-      ...getAttributesOptions,
-      cursor: fromCursor,
-    });
-
-    const TWO = 2;
-    assert.equal(attributesResults.edges.length, TWO);
-    assert.equal(attributesResults.pageInfo.hasNextPage, false);
-    assert.equal(attributesResults.pageInfo.hasPreviousPage, true);
-  });
-
-  it('Should throw error when direction is BACKWARDS and cursor is undefined', async () => {
-    // Add 12 Attributes
-    const attributes = await addTestAttributes(); // tslint:disable-line
-    
-    // Get the first 10 attributes
-    const attributesPromise = service.getAttributes({
-      ...getAttributesOptions,
-      direction: CursorDirection.BACKWARDS,
-    });
-
-    return assertError(NoCursorBackwardsDirection, attributesPromise);
-  });
-
-  it('Should return the previous 2 cursors when direction is BACKWARDS', async () => {
-    // Add 12 Attributes
-    const attributes = await addTestAttributes(); // tslint:disable-line
-
-    // Get the first 10 attributes
-    const attributesResult = await service.getAttributes({
-      ...getAttributesOptions,
-      cursor: fromFirstCursor,
-      direction: CursorDirection.BACKWARDS,
-    });
-
-    const THREE = 3;
-    assert.equal(attributesResult.edges.length, THREE);
-    assert.equal(attributesResult.pageInfo.hasNextPage, true);
-    assert.equal(attributesResult.pageInfo.hasPreviousPage, false);
-  });
-
-  it('should return undefiend cursor, if no attributes', async () => {
-    const attributesResult = await service.getAttributes({
-      ...getAttributesOptions,
-      limit: 1,
-    });
-    assert.equal(attributesResult.pageInfo.endCursor, undefined);
-    assert.equal(attributesResult.pageInfo.startCursor, undefined);
-  });
-
-  it('Should return the previous 1 cursors when limit 1', async () => {
-    await addTestAttributes();
-
-    // Get the first attribute
-    const attributesResult = await service.getAttributes({
-      ...getAttributesOptions,
-      cursor: fromFirstCursor,
-      direction: CursorDirection.BACKWARDS,
-      limit: 1,
-    });
-
-    assert.equal(attributesResult.edges.length, 1);
-    assert.equal(attributesResult.pageInfo.hasNextPage, true);
-    assert.equal(attributesResult.pageInfo.hasPreviousPage, true);
-  });
-
   it('should test $and clause', async () => {
-    await addTestAttributes();
+    await addTestAttributes(service);
 
     const attributesResult = await service.getAttributes({
       ...getAttributesOptions,
@@ -209,8 +76,7 @@ describe('getAttributes', () => {
         value: -1,
       },
     });
-
     assert.equal(attributesResult.edges.length, 1);
     assert.equal(attributesResult.edges[0].node.value, 'brown9');
   });
-}); // tslint:disable-line: max-file-line-count
+});
