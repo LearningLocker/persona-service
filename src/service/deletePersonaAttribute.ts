9df4b325fe1f908bcf03b149b74f8b8f679e406a
James Mullaney <james@ht2.co.uk>
2017-11-28 11:59:16 +0000

    fix(lint): Remove unused import

--- src/service/deletePersonaAttribute.ts
+++ src/service/deletePersonaAttribute.ts
@@ -1,4 +1,3 @@
-import DeletePersonaAttributeOptions from '../serviceFactory/options/DeletePersonaAttributeOptions';
 import Config from './Config';
 
 export default (config: Config) => config.repo.deletePersonaAttribute;
