0ef6f75bf1a7da373319a212e89ec1291778c048
James Mullaney <james@ht2.co.uk>
2017-12-07 13:49:06 +0000

    refactor(repos): Remove memory repo and other unneeded repos

--- src/repoFactory/index.ts
+++ src/repoFactory/index.ts
@@ -1,28 +1,16 @@
 import config from '../config';
-import memoryModelsRepo from '../memoryModelsRepo';
-import Attributes from '../models/Attribute';
-import Identifier from '../models/Identifier';
-import Persona from '../models/Persona';
 import mongoModelsRepo from '../mongoModelsRepo';
 import Repo from './Repo';
 import createMongoClient from './utils/createMongoClient';
 
 export default (): Repo => {
   switch (config.repoFactory.modelsRepoName) {
-    case 'mongo':
+    default: case 'mongo':
       return mongoModelsRepo({
         db: createMongoClient({
           options: config.mongoModelsRepo.options,
           url: config.mongoModelsRepo.url,
         }),
-      });
-    default: case 'memory':
-      return memoryModelsRepo({
-        state: {
-          personaAttributes: [] as Attributes[],
-          personaIdentifiers: [] as Identifier[],
-          personas: [] as Persona[],
-        },
       });
   }
 };
