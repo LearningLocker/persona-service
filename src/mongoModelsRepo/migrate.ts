a3290fc63b82a79a86cf49fd4ddd2d584a83f187
James Mullaney <james@ht2.co.uk>
2018-01-10 16:27:02 +0000

    style: Lint fixes

--- src/mongoModelsRepo/migrate.ts
+++ src/mongoModelsRepo/migrate.ts
@@ -1,7 +1,7 @@
 import Config from './Config';
 import {
   PERSONA_ATTRIBUTES_COLLECTION,
-  PERSONA_IDENTIFIERS_COLLECTION
+  PERSONA_IDENTIFIERS_COLLECTION,
 } from './utils/constants/collections';
 
 export default (config: Config) => {
@@ -15,14 +15,13 @@ export default (config: Config) => {
       ifi: 1,
     }, { unique: true, background: true});
     /* tslint:enable:object-literal-sort-keys */
-    
-    /* tslint:disable:object-literal-sort-keys */
-    attributesCollection.createIndex({ 
-      organisation:1,
-      personaId:1,
-      key:1
-    }, {background:true});
-    /* tslint:enable:object-literal-sort-keys */
 
+    /* tslint:disable:object-literal-sort-keys */
+    await attributesCollection.createIndex({
+      organisation: 1,
+      personaId: 1,
+      key: 1,
+    }, { background: true});
+    /* tslint:enable:object-literal-sort-keys */
   };
 };
