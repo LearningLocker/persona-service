a9b175950f598c994eb1109a8e8e2ce2ba24c4b6
James Mullaney <james@ht2.co.uk>
2018-01-10 16:32:44 +0000

    perf(indexes): Added personaAttributes index (#43)
    * perf(indexes): Added personaAttributes index
    
    * style: Lint fixes

--- src/mongoModelsRepo/migrate.ts
+++ src/mongoModelsRepo/migrate.ts
@@ -1,8 +1,12 @@
 import Config from './Config';
-import { PERSONA_IDENTIFIERS_COLLECTION } from './utils/constants/collections';
+import {
+  PERSONA_ATTRIBUTES_COLLECTION,
+  PERSONA_IDENTIFIERS_COLLECTION,
+} from './utils/constants/collections';
 
 export default (config: Config) => {
   return async () => {
+    const attributesCollection = (await config.db).collection(PERSONA_ATTRIBUTES_COLLECTION);
     const identCollection = (await config.db).collection(PERSONA_IDENTIFIERS_COLLECTION);
 
     /* tslint:disable:object-literal-sort-keys */
@@ -12,5 +16,12 @@ export default (config: Config) => {
     }, { unique: true, background: true});
     /* tslint:enable:object-literal-sort-keys */
 
+    /* tslint:disable:object-literal-sort-keys */
+    await attributesCollection.createIndex({
+      organisation: 1,
+      personaId: 1,
+      key: 1,
+    }, { background: true});
+    /* tslint:enable:object-literal-sort-keys */
   };
 };
