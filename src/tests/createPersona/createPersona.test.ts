dbeb6382ff4eb7279099236dfef5a3a9ee21b6f4
Eugene <yevhen.sharpinskyi@gmail.com>
2020-03-27 21:05:52 +0200

    test(CreatePersona): Create test

--- src/tests/createPersona/createPersona.test.ts
+++ src/tests/createPersona/createPersona.test.ts
@@ -1,5 +1,6 @@
 import * as assert from 'assert';
 import createTestPersona from '../utils/createTestPersona';
+import createTestPersonaWithoutName from '../utils/createTestPersonaWithoutName';
 import setup from '../utils/setup';
 import { TEST_ORGANISATION } from '../utils/values';
 
@@ -16,6 +17,18 @@ describe('createPersona', () => {
 
     assert.equal(actualPersona.id, persona.id);
     assert.equal(actualPersona.name, 'Dave');
+    assert.equal(actualPersona.organisation, TEST_ORGANISATION);
+  });
+
+  it('Should create a persona with personaId instead name, if name not exists.', async () => {
+    const persona = await createTestPersonaWithoutName();
+    const {persona: actualPersona} = await service.getPersona({
+      organisation: TEST_ORGANISATION,
+      personaId: persona.id,
+    });
+
+    assert.equal(actualPersona.id, persona.id);
+    assert.equal(actualPersona.name, persona.id.toString());
     assert.equal(actualPersona.organisation, TEST_ORGANISATION);
   });
 });
