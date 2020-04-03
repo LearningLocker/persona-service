178726dffe8b9b99e8d3aac473081caa1945e2b8
Yevhen <yevhen.sharpinskyi@gmail.com>
2020-04-03 15:39:39 +0200

    fix: Uses persona ID for personas without a name (LLC-57) (#293)

--- src/tests/utils/createTestPersonaWithoutName.ts
+++ src/tests/utils/createTestPersonaWithoutName.ts
@@ -3,9 +3,8 @@ import PersonaModel from '../../models/Persona';
 import service from '../../tester';
 import { TEST_ORGANISATION } from './values';
 
-export default async (name?: string, organisation: string = TEST_ORGANISATION) => {
+export default async (organisation: string = TEST_ORGANISATION) => {
   const {persona} = await service.createPersona({
-    name: name === undefined ? 'Dave' : name,
     organisation,
   });
   return persona;
