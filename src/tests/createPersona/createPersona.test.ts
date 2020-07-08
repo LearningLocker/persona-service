5eccf3ecb7d251aced3677f4c0e90a648afd3564
cbishopvelti <chrisjbishop155@gmail.com>
2020-07-08 15:09:44 +0100

    fix: Allows persona creation without name (LLC-57) (#305)

--- src/tests/createPersona/createPersona.test.ts
+++ src/tests/createPersona/createPersona.test.ts
@@ -1,11 +1,9 @@
 import * as assert from 'assert';
 import createTestPersona from '../utils/createTestPersona';
-import createTestPersonaWithoutName from '../utils/createTestPersonaWithoutName';
 import setup from '../utils/setup';
 import { TEST_ORGANISATION } from '../utils/values';
 
 describe('createPersona', () => {
-
   const service = setup();
 
   it('Should create persona', async () => {
@@ -17,18 +15,6 @@ describe('createPersona', () => {
 
     assert.equal(actualPersona.id, persona.id);
     assert.equal(actualPersona.name, 'Dave');
-    assert.equal(actualPersona.organisation, TEST_ORGANISATION);
-  });
-
-  it('Should create a persona with personaId instead name, if name not exists.', async () => {
-    const persona = await createTestPersonaWithoutName();
-    const {persona: actualPersona} = await service.getPersona({
-      organisation: TEST_ORGANISATION,
-      personaId: persona.id,
-    });
-
-    assert.equal(actualPersona.id, persona.id);
-    assert.equal(actualPersona.name, persona.id.toString());
     assert.equal(actualPersona.organisation, TEST_ORGANISATION);
   });
 });
