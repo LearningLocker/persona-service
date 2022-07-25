c4737d433c379a65cf85d5d4089c85f3d135dd61
Toran Sharma <toransharma27@gmail.com>
2022-07-25 15:53:48 +0100

    refactor(Locked Error): Overwrite BaseError message field for Locked Error

--- src/errors/Locked.ts
+++ src/errors/Locked.ts
@@ -3,7 +3,10 @@ import BaseError from 'jscommons/dist/errors/BaseError';
 import Identifier from '../models/Identifier';
 
 export default class extends BaseError {
+  message: string;
+
   constructor(public identifier: Identifier) {
     super();
+    this.message = 'Locked';
   }
 }
